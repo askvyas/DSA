fe1706581a8c3706849e2bdd5e0353476bdfd8ab
A SAI KARTHIK VYAS <vyasakondi009@gmail.com>
2020-09-02 21:34:42 +0530

    Arrays cmt 10

--- Arrays/TriangularMat.java
+++ Arrays/TriangularMat.java
@@ -6,6 +6,9 @@ class TriangularMat
     int m=sc.nextInt();
     int n=sc.nextInt();
     int[][] arr=new int[m][n];
+    int[][] arr1=new int[m][n];
+    int[][] arr2=new int[m][n];
+
     for (int i=0;i<m;i++) {
       for (int j=0;j<n;j++) {
         arr[i][j]=sc.nextInt();
@@ -13,14 +16,39 @@ class TriangularMat
       }
     }
     for (int i=0;i<m;i++) {
-      for (int j=m-i;j<n;j++) {
-        arr[j][i]=0;
+      for (int j=0;j<n;j++) {
+        if(j>i)
+        {
+          arr1[i][j]=0;
+        }
+        else {
+          arr1[i][j]=arr[i][j];
+        }
 
       }
     }
     for (int i=0;i<m;i++) {
       for (int j=0;j<n;j++) {
-        System.out.print(arr[i][j]+" ");
+        if(j<i)
+        {
+          arr2[i][j]=0;
+        }
+        else {
+          arr2[i][j]=arr[i][j];
+        }
+
+      }
+    }
+    for (int i=0;i<m;i++) {
+      for (int j=0;j<n;j++) {
+        System.out.print(arr1[i][j]+" ");
+
+      }
+      System.out.println();
+    }
+    for (int i=0;i<m;i++) {
+      for (int j=0;j<n;j++) {
+        System.out.print(arr2[i][j]+" ");
 
       }
       System.out.println();
