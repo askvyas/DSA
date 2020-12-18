fb3d29482e10714eefcc31128c8a10e8854ea176
A SAI KARTHIK VYAS <vyasakondi009@gmail.com>
2020-12-18 21:54:53 +0530

    Arrays Added

--- Recursion/Binary.java
+++ Recursion/Binary.java
@@ -13,15 +13,46 @@ n
 import java.util.*;
 class Binary
 {
+ public static void binary(int[] arr,int n,int k)
+ {
+   //String s = Arrays.toString(arr).replaceAll("\\[|\\]|,|\\s", "");
 
+   //System.out.print(s);
+   if(k==n)
+   {
+     String s = Arrays.toString(arr).replaceAll("\\[|\\]|,|\\s", "");
+
+     System.out.println(s);
+     return ;
+   }
+   if(arr[k-1]==0)
+   {
+     arr[k]=0;
+     binary(arr,n,k+1);
+     arr[k]=1;
+     binary(arr,n,k+1);
+
+
+   }
+   else {
+     arr[k]=0;
+     binary(arr,n,k+1);
+
+   }
+ }
 
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int t =sc.nextInt();
     while (t-->0) {
       int n=sc.nextInt();
-      String s="0";
-      System.out.println();
+      int[] arr=new int[n];
+      arr[0]=0;
+
+      binary(arr,n,1);
+      arr[0]=1;
+      binary(arr,n,1);
+
 
     }
 
